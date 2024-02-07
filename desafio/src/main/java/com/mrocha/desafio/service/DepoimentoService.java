ca407f492625a47b08892acbd5b073b73d061d8e
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-02-07 18:06:35 -0300

    Atualizando os metodos do de buscarTodos, buscarPorId e criarDepoimento

--- desafio/src/main/java/com/mrocha/desafio/service/DepoimentoService.java
+++ desafio/src/main/java/com/mrocha/desafio/service/DepoimentoService.java
@@ -1,6 +1,7 @@
 package com.mrocha.desafio.service;
 
 import com.mrocha.desafio.dto.DepoimentoDTO;
+import com.mrocha.desafio.model.Depoimento;
 import com.mrocha.desafio.repository.DepoimentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,22 +15,26 @@ public class DepoimentoService {
     @Autowired
     private DepoimentoRepository depoimentoRepository;
 
-    public List<DepoimentoDTO> buscarTodosDepoimentos() {
+    public List<Depoimento> buscarTodosDepoimentos() {
+        List<Depoimento> depoimentos = depoimentoRepository.findAll();
+        return depoimentos;
     }
 
-    public DepoimentoDTO buscarDepoimentoPorId() {
+    public Optional<Depoimento> buscarDepoimentoPorId(Long id) {
+        Optional<Depoimento> depoimentoId = depoimentoRepository.findById(id);
+        return depoimentoId;
+    }
+
+    public Depoimento criarDepoimento(Depoimento depoimento) {
+        Depoimento novoDepoimento = depoimentoRepository.save(depoimento);
+        return novoDepoimento;
+    }
+
+    public Depoimento atualizarDepoimento() {
 
     }
 
-    public DepoimentoDTO criarDepoimento() {
-
-    }
-
-    public DepoimentoDTO atualizarDepoimento() {
-
-    }
-
-    public DepoimentoDTO deletarDepoimento() {
+    public Depoimento deletarDepoimento() {
 
     }
 }
