c1e04e5a10654ffdc11c190c8a7f2808c6e4cf91
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-02-08 17:27:34 -0300

    Atualizando os metodos do de buscarTodosDepoimentos e buscarDepoimentoPorId no conrtroller

--- desafio/src/main/java/com/mrocha/desafio/controller/DepoimentoController.java
+++ desafio/src/main/java/com/mrocha/desafio/controller/DepoimentoController.java
@@ -1,4 +1,37 @@
 package com.mrocha.desafio.controller;
 
+import com.mrocha.desafio.dto.DepoimentoDTO;
+import com.mrocha.desafio.model.Depoimento;
+import com.mrocha.desafio.service.DepoimentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("depoimentos")
 public class DepoimentoController {
+
+    @Autowired
+    private DepoimentoService service;
+
+    @GetMapping
+    public ResponseEntity<List<Depoimento>> buscarTodosDepoimentos() {
+        return ResponseEntity.ok(service.buscarTodosDepoimentos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Depoimento> buscarDepoimentoPorId(@PathVariable  Long id) {
+        Optional<Depoimento> response = service.buscarDepoimentoPorId(id);
+        if (response.isPresent()) {
+            return ResponseEntity.ok(response.get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+
 }
