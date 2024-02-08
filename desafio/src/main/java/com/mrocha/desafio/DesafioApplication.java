2b26b8ca96a34dab3970e6ae5f5ab54caa66c33d
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-02-08 18:22:02 -0300

    Adicionando SecurityAutoConfiguration

--- desafio/src/main/java/com/mrocha/desafio/DesafioApplication.java
+++ desafio/src/main/java/com/mrocha/desafio/DesafioApplication.java
@@ -2,10 +2,11 @@ package com.mrocha.desafio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 //'g G h H ""'
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class DesafioApplication {
 
 	public static void main(String[] args) {
