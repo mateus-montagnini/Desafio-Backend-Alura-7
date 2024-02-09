647008f330c164f3f6ad1ea8d089619d6989ae8c
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-02-09 17:32:15 -0300

    Criando metodo para buscar tre depoimentos aleatorios no service

--- desafio/src/main/java/com/mrocha/desafio/service/DepoimentoService.java
+++ desafio/src/main/java/com/mrocha/desafio/service/DepoimentoService.java
@@ -1,16 +1,18 @@
 package com.mrocha.desafio.service;
 
-import com.mrocha.desafio.dto.DepoimentoDTO;
 import com.mrocha.desafio.model.Depoimento;
 import com.mrocha.desafio.repository.DepoimentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class DepoimentoService {
+
+    private RestTemplate restTemplate;
 
     @Autowired
     private DepoimentoRepository depoimentoRepository;
@@ -42,5 +44,24 @@ public class DepoimentoService {
             return true;
         }
         return false;
+    }
+
+    public List<Depoimento> buscarTresDepoimentos() {
+        List<Depoimento> depoimentos = depoimentoRepository.findAll();
+        if (depoimentos.isEmpty()) {
+            return depoimentos;
+        }
+        shuffleList(depoimentos, new Random());
+
+        return depoimentos.subList(0, Math.min(depoimentos.size(), 3));
+    }
+
+    private <T> void shuffleList(List<T> list, Random random) {
+        for (int i = list.size() - 1; i > 0; i--) {
+            int index = random.nextInt(i + 1);
+            T temp = list.get(index);
+            list.set(index, list.get(i));
+            list.set(i, temp);
+        }
     }
 }
