4a1e6c8040c5315a5b1677746493fab34688b79f
mateus-montagnini <mateus.r.montagnini@gmail.com>
2024-02-10 16:51:27 -0300

    Refatorando o DepoimentoController

--- desafio/src/main/java/com/mrocha/desafio/controller/DepoimentoController.java
+++ desafio/src/main/java/com/mrocha/desafio/controller/DepoimentoController.java
@@ -2,31 +2,38 @@ package com.mrocha.desafio.controller;
 
 import com.mrocha.desafio.dto.DepoimentoDTO;
 import com.mrocha.desafio.model.Depoimento;
+import com.mrocha.desafio.model.DepoimentoFoto;
+import com.mrocha.desafio.service.DepoimentoFotoService;
 import com.mrocha.desafio.service.DepoimentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("depoimentos")
+@RequestMapping("/depoimentos")
 public class DepoimentoController {
 
     @Autowired
     private DepoimentoService service;
 
+    @Autowired
+    private DepoimentoFotoService serviceFoto;
+
     @GetMapping
-    public ResponseEntity<List<Depoimento>> buscarTodosDepoimentos() {
+    public ResponseEntity<List<DepoimentoDTO>> buscarTodosDepoimentos() {
         return ResponseEntity.ok(service.buscarTodosDepoimentos());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Depoimento> buscarDepoimentoPorId(@PathVariable("id")  Long id) {
-        Optional<Depoimento> response = service.buscarDepoimentoPorId(id);
+    public ResponseEntity<DepoimentoDTO> buscarDepoimentoPorId(@PathVariable("id")  Long id) {
+        Optional<DepoimentoDTO> response = service.buscarDepoimentoPorId(id);
         if (response.isPresent()) {
             return ResponseEntity.ok(response.get());
         }
@@ -34,8 +41,8 @@ public class DepoimentoController {
     }
 
     @PostMapping
-    public ResponseEntity<Depoimento> criarDepoimento(@RequestBody Depoimento depoimento) {
-        Optional<Depoimento> novoDepoimento = service.criarDepoimento(depoimento);
+    public ResponseEntity<DepoimentoDTO> criarDepoimento(@RequestBody DepoimentoDTO depoimento) throws IOException {
+        Optional<DepoimentoDTO> novoDepoimento = service.criarDepoimento(depoimento);
         if (novoDepoimento.isPresent()) {
             return new ResponseEntity<>(novoDepoimento.get(), HttpStatus.CREATED);
         }
@@ -43,9 +50,9 @@ public class DepoimentoController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Depoimento> atualizarDepoimento(@PathVariable("id") Long id,
-                                                          @RequestBody Depoimento depoimento) {
-        Optional<Depoimento> depoimentoAtualizado = service.atualizarDepoimento(depoimento);
+    public ResponseEntity<DepoimentoDTO> atualizarDepoimento(@PathVariable("id") Long id,
+                                                          @RequestBody DepoimentoDTO depoimento) {
+        Optional<DepoimentoDTO> depoimentoAtualizado = service.atualizarDepoimento(id, depoimento);
         if (depoimentoAtualizado.isPresent()) {
             return ResponseEntity.ok(depoimentoAtualizado.get());
         }
@@ -61,10 +68,5 @@ public class DepoimentoController {
         }
 
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-    @GetMapping("depoimentos-home")
-    public ResponseEntity<List<Depoimento>> buscarTresDepoimentos() {
-        return ResponseEntity.ok(service.buscarTresDepoimentos());
     }
 }
